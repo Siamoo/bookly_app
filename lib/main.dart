3c72f5f9634bf498f81ee0b2d6ed24c27b12e7e7
siam <walidsyam.f@gmail.com>
2024-08-03 22:21:47 +0300

    provide cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/presntation/manger/bottom_books_cubit/botttom_books_cubit.dart';
+import 'package:bookly_app/Features/home/presntation/manger/top_books_cubit/top_books_cubit.dart';
 import 'package:bookly_app/Features/home/presntation/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presntation/views/home_view.dart';
 import 'package:bookly_app/Features/search/presntation/views/search_view.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -13,14 +21,25 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        'BookDetailsView': (context) => const BookDetailsView(),
-        'SearchView': (context) => const SearchView(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
-      home: const HomeView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => TopBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => BotttomBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp(
+        routes: {
+          'BookDetailsView': (context) => const BookDetailsView(),
+          'SearchView': (context) => const SearchView(),
+        },
+        debugShowCheckedModeBanner: false,
+        theme:
+            ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+        home: const HomeView(),
+      ),
     );
   }
 }
